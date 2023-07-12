ab8c7bd9ffb68f6109d82247a1798ba9507b34f6
SupeerSanny <ksanny556@gmail.com>
2023-07-12 16:27:03 +0530

    responsive header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,42 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { images } from "../constants";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const navItemsInfo = [
-  { name: "Home" },
-  { name: "Aticals" },
-  { name: "Pages" },
-  { name: "Pricing" },
-  { name: "Faqs" },
+  { name: "Home", type: "link" },
+  { name: "Article", type: "link" },
+  { name: "Pages", type: "dropdown", items: ["About us", "Contact us"] },
+  { name: "Pricing", type: "link" },
+  { name: "Faqs", type: "link" },
 ];
 
-const NavItem = ({ name }) => {
+const NavItem = ({ item }) => {
   return (
     <li className="relative group">
-      <a href="/" className="px-4 py-2">
-        {name}
-      </a>
-      <span className="text-orange-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
-        |
-      </span>
+      {item.type === "link" ? (
+        <>
+          <a href="/" className="px-4 py-2">
+            {item.name}
+          </a>
+          <span className="text-orange-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+            |
+          </span>
+        </>
+      ) : (
+        <>
+          <a href="/" className="px-4 py-2 flex gap-x-1 items-center">
+            <span>{item.name}</span>
+            <MdKeyboardArrowDown />
+          </a>
+          <div className="hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 trasnform translate-y-full group-hover:block w-max">
+            <ul className="flex flex-col shadow-lg rounded-lg overflow-hidden">
+              {item.items.map((page) => (
+                <a
+                  href="/"
+                  className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-orange-500 "
+                >
+                  {page}
+                </a>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </li>
   );
 };
 
 const Header = () => {
+  const [navIsVisible, setNavIsVisible] = useState(false);
+  const navVisibilityHandler = () => {
+    setNavIsVisible((curState) => {
+      return !curState;
+    });
+  };
   return (
     <>
       <section>
         <header className="container mx-auto px-5 flex justify-between py-2 items-center">
           <div className="">
-            <img src={images.Logo} alt="logo" />
+            <img className="w-16" src={images.Logo} alt="logo" />
           </div>
-          <div className="flex gap-x-7 items-center font-semibold">
-            <ul className="flex gap-x-3">
+          <div className="lg:hidden z-50">
+            {navIsVisible ? (
+              <AiOutlineClose
+                className="w-6 h-6"
+                onClick={navVisibilityHandler}
+              />
+            ) : (
+              <AiOutlineMenu
+                className="w-6 h-6"
+                onClick={navVisibilityHandler}
+              />
+            )}
+          </div>
+          <div
+            className={`${
+              navIsVisible ? "right-0" : "-right-full"
+            } transition-all duration-500 mt-[80px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top- bottom-0 flex lg:static gap-x-9 gap-x-7 items-center font-semibold`}
+          >
+            <ul className=" text-white items-center gap-y-5 lg:text-secondary flex flex-col lg:flex-row  gap-x-2">
               {navItemsInfo.map((item) => (
-                <NavItem key={item.name} name={item.name} />
+                <NavItem key={item.name} item={item} />
               ))}
             </ul>
-            <button className="border-2 border-orange-600 px-6 py-2 rounded-full text-orange-600 hover:bg-orange-600 hover:text-white transition-all duration-300">
+            <button className="mt-5 lg:mt-0 border-2 border-orange-600 px-6 py-2 rounded-full text-orange-600 hover:bg-orange-600 hover:text-white transition-all duration-300">
               Sign in
             </button>
           </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#132d46",
+        secondary: "#01c38d",
+        dark: {
+          hard: "#191E29",
+          soft: "#696e79",
+        },
+      },
+      fontFamily: {
+        opensans: ["'Open Sans'", "'sans-serif'"],
+        roboto: ["'Roboto'", "'sans-serif'"],
+      },
+    },
   },
   plugins: [],
 };
