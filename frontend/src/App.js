6f806c006c3d21cb0854c90a748da57f6bceee0f
Sanny Kumar <ksanny556@gmail.com>
2023-07-15 23:43:38 +0530

    Article Details Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
+import ArticleDetailsPage from "./pages/ArticleDetails/ArticleDetailsPage";
 
 function App() {
   return (
     <div className="App font-opensans">
-      <HomePage />
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/blog/:id" element={<ArticleDetailsPage />} />
+      </Routes>
     </div>
   );
 }
